f4a9a79455e2135f891dbc7376f5749c2d0edfd4
Arjan Speiard | Asgard Sings! <arjan.spieard@gmail.com>
2020-03-20 00:19:33 +0100

    Removed useless stuff in the repo

--- autogen.sh
+++ autogen.sh
@@ -33,38 +33,6 @@ if [ ! -z "$@" ]; then
   done
 fi
 
-
-# Check that we're being run from the right directory.
-if test ! -f compact_enc_det/compact_enc_det.h; then
-  cat >&2 << __EOF__
-Could not find source code.  Make sure you are running this script from the
-root of the distribution tree.
-__EOF__
-  exit 1
-fi
-
-# Check that gtest is present. It is used to build unit test suite.
-if test ! -e gtest; then
-  if test -z $(which curl); then
-    echo "curl cannot be found. Please install it to build the package."
-    exit 1
-  fi
-
-  echo "Google Test not present.  Fetching from the web..."
-  curl $curlopts -L -O https://github.com/google/googletest/archive/master.zip
-  unzip -q master.zip
-  rm master.zip
-  mv googletest-master gtest
-fi
-
-if test -z $(which cmake); then
-  echo "CMake cannot be found. Please install it to build the package."
-  exit 1
-fi
-
-# Build gtest
-(cd gtest && cmake . && make)
-
 # Build the main package
 cmake . && make
 
